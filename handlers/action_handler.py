1fa3bcf735bd62e7af6d0e2c22afbd101ed95e70
wangwenqiang <wangwenqiang@metrodata.cn>
2017-12-29 14:57:14 +0800

    2017年12月29日14:57:12

--- handlers/action_handler.py
+++ handlers/action_handler.py
@@ -6,8 +6,9 @@ from tornado import web
 
 class ActionHandler(tornado.web.RequestHandler):
     # 每个行为刷新历史行为
-    def initialize(self):
-        pass
+    def initialize(self, action_data, stages):
+        self.actions = action_data
+        self.stages = stages
 
     def get(self):
         pass
--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from configparser import ExtendedInterpolation
 from handlers.fresh_handler import FreshHandler
 from handlers.main_handler import MainHandler
 from handlers.test_handler import TestHandler
+from handlers.action_handler import ActionHandler
 from tool.path import one_level
 from tornado import httpserver, ioloop, web, netutil
 
@@ -19,15 +20,23 @@ def main_run():
         cf.read(conf_file)
         data_file = cf.get('sample', 'file_dir')
         score_file = cf.get('sample', 'score_tag_file')
+        action_file = cf.get('sample', 'action_file')
     except Exception as e:
         raise e
 
     app = tornado.web.Application(
         [
             (r'/home', MainHandler,
-             {'data': pd.read_csv(data_file),
-              'prepare_score_dict': joblib.load(score_file),
-              }
+             {
+                 'data': pd.read_csv(data_file),
+                 'prepare_score_dict': joblib.load(score_file),
+             }
+             ),
+            (r'/act', ActionHandler,
+             {
+                 'action_data': joblib.load(action_file),
+                 'stages': ['初识期', '探索期', '发展期', '稳定期', '恶化期']
+             }
              ),
             (r'/fresh', FreshHandler,
              ),
