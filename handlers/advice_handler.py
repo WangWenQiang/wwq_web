c42f608c6f1e52ed1f384c43e9f1094c12a42b12
wangwenqiang <wangwenqiang@metrodata.cn>
2018-01-10 11:54:21 +0800

    2018-01-10 11:54:20

--- handlers/advice_handler.py
+++ handlers/advice_handler.py
@@ -10,15 +10,15 @@ class AdviceHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
         self.get_param = {k: str(v[0], encoding="utf-8") for k, v in self.request.arguments.items()}
         sample_id = self.get_param['sampleID']
-        now_stage = self.get_param['当前阶段']
+        now_stage = self.get_param['now_stage']
 
         self.get_param.pop('sampleID')
-        self.get_param.pop('当前阶段')
+        self.get_param.pop('now_stage')
 
         for k, v in (self.get_param).items():
             self.get_param[k] = avearge_score(v)
         self.get_param['created'] = get_now_datetime()
-        self.get_param['当前阶段'] = now_stage
+        self.get_param['所选阶段'] = now_stage
 
         actions_info = db_link['zz_wenjuan'].find_one({'sample_index': int(sample_id)})
         actions_list = actions_info.get('actions', [])
--- handlers/score_handler.py
+++ handlers/score_handler.py
@@ -11,9 +11,9 @@ class ScoreHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
         self.get_param = {k: str(v[0], encoding="utf-8") for k, v in self.request.arguments.items()}
         sample_id = self.get_param['sampleID']
-        now_stage = self.get_param['当前阶段']
+        now_stage = self.get_param['now_stage']
         self.get_param.pop('sampleID')
-        self.get_param.pop('当前阶段')
+        self.get_param.pop('now_stage')
         for k, v in (self.get_param).items():
             self.get_param[k] = avearge_score(v)
         self.get_param['score_time'] = get_now_datetime()
